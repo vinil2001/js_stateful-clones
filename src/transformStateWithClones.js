3f06732086c758b77c0db45964cd0b4711a363e7
ABOO <aboo@xpandsoftware.com>
2025-07-01 12:47:21 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -4,10 +4,33 @@
  * @param {Object} state
  * @param {Object[]} actions
  *
- * @return {Object[]}
+ * @return {Object[]} Array of new state snapshots after each action
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const snapshots = [];
+
+  actions.forEach(({ type, extraData, keysToRemove }) => {
+    switch (type) {
+      case 'addProperties':
+        currentState = { ...currentState, ...extraData };
+        break;
+      case 'removeProperties':
+        currentState = { ...currentState };
+
+        keysToRemove.forEach((key) => {
+          delete currentState[key];
+        });
+        break;
+      case 'clear':
+        currentState = {};
+        break;
+    }
+
+    snapshots.push({ ...currentState });
+  });
+
+  return snapshots;
 }
 
 module.exports = transformStateWithClones;
